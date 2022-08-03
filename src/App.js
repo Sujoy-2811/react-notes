353d7cd5c1220aafae961ac07f895c3866698589
Shubham-0711 <90834170+Shubham-0711@users.noreply.github.com>
2022-08-03 19:26:48 +0530

    Included css

--- src/App.js
+++ src/App.js
@@ -6,31 +6,32 @@ import InputForm from "./InputForm/InputForm.js";
 import List from "./InputForm/LIst/List";
 
 function App() {
-  const [data, setData] = useState([{title : "hello" , note : "This is my note"}]);
+  const [data, setData] = useState([
+    { title: "hello", note: "This is my note" },
+  ]);
 
-  const deleteHandle = (val) =>{
-    setData((predata) =>{
-      return predata.filter((item)=> item !== val)
-    })
+  const deleteHandle = (val) => {
+    setData((predata) => {
+      return predata.filter((item) => item !== val);
+    });
     // console.log(" button pressed");
-  }
+  };
 
   const addData = (item) => {
     setData((preData) => {
-
-      if(preData.length === 0){
- return [ item];
+      if (preData.length === 0) {
+        return [item];
       }
       return [item, ...preData];
     });
   };
   return (
     <React.Fragment>
-      <header >
+      <header>
         <h1>React Notes</h1>
       </header>
       <InputForm dataHandler={addData}></InputForm>
-      <List data={data}  deleteHandle={deleteHandle}></List>
+      <List data={data} deleteHandle={deleteHandle}></List>
     </React.Fragment>
   );
 }
