4090b7b8e19b95b3ed712b87a593cfbe9c36b262
Sujoy Manna <sujoymanna007@gmail.com>
2022-08-09 21:53:55 +0530

    changed demo data

--- src/App.js
+++ src/App.js
@@ -9,19 +9,19 @@ import styles from "./App.module.css";
 
 function App() {
   const [data, setData] = useState([
-    { title: "null", note: "This is my note"  },
-    { title: "1278901234", note: "This is my note", col : 0  },
-    { title: "123478901234", note: "This is my note"  , col : 1 },
-    { title: "12345678901234", note: "This is my note"  , col : 2 },
-    { title: "hel1los", note: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id voluptate, molestiae rem quod ducimus veritatisff"  , col : 1},
-    { title: "hellosl", note: "daLorem ipsum dolor sit amet consectetur adipisicing elit. Id voluptate, molestiae rem quod ducimus veritatisff" , col : 3 },
+    { title: "hi", note: "This is my note"  },
+    { title: "hello", note: "This is my noteThis is my noteThis is my noteThis is my note", col : 1  },
+    { title: "127890fvd1234", note: "This is my note", col : 2  },
+    { title: "12789vdcxv01234", note: "This is my note", col : 3  },
+    { title: "1278dv901234", note: "This is my note", col : 2  },
+    { title: "12789dx01234", note: "This is my note", col : 3  },
+    { title: "1278d901234", note: "This is my note", col : 0  },
   ]);
 
   const deleteHandle = (val) => {
     setData((predata) => {
       return predata.filter((item) => item !== val);
     });
-    // console.log(" button pressed");
   };
 
   const addData = (item) => {
